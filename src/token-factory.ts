5c312e93ba9fbc6d49ae67e89a24367362d62b28
danilych <danilych45@proton.me>
2025-03-23 07:20:37 +0100

    feat: add omnichain token created event

--- src/token-factory.ts
+++ src/token-factory.ts
@@ -1,8 +1,9 @@
 import {
   OwnershipTransferred as OwnershipTransferredEvent,
-  TokenCreated as TokenCreatedEvent
+  TokenCreated as TokenCreatedEvent,
+  OmnichainTokenCreated as OmnichainTokenCreatedEvent
 } from "../generated/TokenFactory/TokenFactory"
-import { OwnershipTransferred, TokenCreated } from "../generated/schema"
+import { OwnershipTransferred, TokenCreated, OmnichainTokenCreated } from "../generated/schema"
 
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
@@ -22,6 +23,25 @@ export function handleOwnershipTransferred(
 
 export function handleTokenCreated(event: TokenCreatedEvent): void {
   let entity = new TokenCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.token = event.params.token
+  entity.name = event.params.name
+  entity.ticker = event.params.ticker
+  entity.initialAmount = event.params.initialAmount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOmnichainTokenCreated(
+  event: OmnichainTokenCreatedEvent
+): void {
+  let entity = new OmnichainTokenCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.owner = event.params.owner
